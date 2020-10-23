5dde0a074bbf8c92b2f8f03be4b645124bb552f0
wdsondallas <wdson_henrique@hotmail.com>
2020-10-23 17:18:31 -0300

    atualizacao

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./components/styles.css";
 import { CardVideo } from "./components/CardVideo";
+//import styled from 'styled-components';
 //import { Topico } from "./components/Topico";
 import { Header } from "./components/Header";
 
@@ -26,35 +27,51 @@ export default function App() {
           <section class="painel-de-videos">
             <CardVideo
               url={"https://picsum.photos/400/400?a=1"}
-              tituloDoVideo={"Item E"}
+              tituloDoVideo={"Item A"}
+              precoDoVideo={"R$ 40.50"}
+              adicionarCarrinho={"Adicionar ao Carrinho"}
             />
             <CardVideo
               url={"https://picsum.photos/400/400?a=2"}
-              tituloDoVideo={"Item D"}
+              tituloDoVideo={"Item B"}
+              precoDoVideo={"R$ 55.90"}
+              adicionarCarrinho={"Adicionar ao Carrinho"}
             />
             <CardVideo
               url={"https://picsum.photos/400/400?a=3"}
-              tituloDoVideo={"Item B"}
+              tituloDoVideo={"Item C"}
+              precoDoVideo={"R$ 80.00"}
+              adicionarCarrinho={"Adicionar ao Carrinho"}
             />
             <CardVideo
               url={"https://picsum.photos/400/400?a=4"}
-              tituloDoVideo={"Item C"}
+              tituloDoVideo={"Item D"}
+              precoDoVideo={"R$ 99.00"}
+              adicionarCarrinho={"Adicionar ao Carrinho"}
             />
             <CardVideo
               url={"https://picsum.photos/400/400?a=5"}
-              tituloDoVideo={"Título 4"}
+              tituloDoVideo={"Item E"}
+              precoDoVideo={"R$ 199.00"}
+              adicionarCarrinho={"Adicionar ao Carrinho"}
             />
             <CardVideo
               url={"https://picsum.photos/400/400?a=6"}
-              tituloDoVideo={"Título 5"}
+              tituloDoVideo={"Item F"}
+              precoDoVideo={"R$ 210.00"}
+              adicionarCarrinho={"Adicionar ao Carrinho"}
             />
             <CardVideo
               url={"https://picsum.photos/400/400?a=7"}
-              tituloDoVideo={"Título 6"}
+              tituloDoVideo={"Item G"}
+              precoDoVideo={"R$ 499.99"}
+              adicionarCarrinho={"Adicionar ao Carrinho"}
             />
             <CardVideo
               url={"https://picsum.photos/400/400?a=8"}
-              tituloDoVideo={"Título 7"}
+              tituloDoVideo={"Item H"}
+              precoDoVideo={"R$ 501.00"}
+              adicionarCarrinho={"Adicionar ao Carrinho"}
             />
           </section>
         </main>
